3f7cc812e9912d1fb7e352c79bcb31753f9203c6
Fang Yuan <fayndee@hotmail.com>
2017-08-24 15:27:05 +0200

    Add basic project skeleton.

---
+++ src/main/scala/RomanNumerals.scala
@@ -0,0 +1,5 @@
+
+object RomanNumerals {
+
+  def toRoman(number: Int): String = ???
+}
---
+++ src/test/scala/RomanNumeralsTest.scala
@@ -0,0 +1,12 @@
+import org.scalatest.{FlatSpec, Matchers}
+
+class RomanNumeralsTest extends FlatSpec with Matchers {
+
+  "Some function" should "throw exception when 0 is given" in {
+
+  }
+
+  it should "return 'I' when 1 is given" in {
+    
+  }
+}
